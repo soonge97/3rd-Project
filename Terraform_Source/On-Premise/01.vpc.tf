9148ee2b2098132c561b7f7ab720c1a2bcc6625d
soonge97 <ljh9795@naver.com>
2024-11-22 04:11:45 +0900

    Delete Terraform_Source/On-Premise directory

--- Terraform_Source/On-Premise/01.vpc.tf
+++
@@ -1,9 +0,0 @@
-# VPC 생성
-resource "aws_vpc" "ON-PREMISE-VPC" {
-  cidr_block           = "10.240.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    "Name" = "ON-PREMISE-VPC"
-  }
-}
--- Terraform_Source/On-Premise/05.ig.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "ON-PREMISE-IGW" {
-  vpc_id = aws_vpc.ON-PREMISE-VPC.id
-  tags = {
-    "Name" = "ON-PREMISE-IGW"
-  }
-}
